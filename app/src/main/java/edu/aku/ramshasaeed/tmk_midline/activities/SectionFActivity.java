ba63d443cb09416f234b6790a597d7d3359e1414
ramsha.ahmed <ramshasaeed2016@gmail.com>
2019-01-28 16:05:49 +0500

    Added section A

--- app/src/main/java/edu/aku/ramshasaeed/tmk_midline/activities/SectionFActivity.java
+++ app/src/main/java/edu/aku/ramshasaeed/tmk_midline/activities/SectionFActivity.java
@@ -3,6 +3,8 @@ package edu.aku.ramshasaeed.tmk_midline.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.aku.ramshasaeed.tmk_midline.R;
+
 public class SectionFActivity extends AppCompatActivity {
 
     @Override
