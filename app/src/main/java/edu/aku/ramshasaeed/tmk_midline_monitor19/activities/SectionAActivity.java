d5a8929267410dcabb8e8995f1740ffc76c98fcc
ali.azaz <ali.azaz.alam@hotmail.com>
2019-11-25 12:03:57 +0500

    initial commit

---
+++ app/src/main/java/edu/aku/ramshasaeed/tmk_midline_monitor19/activities/SectionAActivity.java
@@ -0,0 +1,390 @@
+package edu.aku.ramshasaeed.tmk_midline_monitor19.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.IdRes;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.aku.ramshasaeed.tmk_midline_monitor19.R;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.contracts.BLRandomContract;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.contracts.FormsContract;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.contracts.VillagesContract;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.core.DatabaseHelper;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.core.MainApp;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.databinding.ActivitySectionABinding;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.validation.ClearClass;
+import edu.aku.ramshasaeed.tmk_midline_monitor19.validation.ValidatorClasss;
+
+public class SectionAActivity extends Activity {
+    ActivitySectionABinding bi;
+    private static final String TAG = SectionAActivity.class.getName();
+    String dtToday = new SimpleDateFormat("dd-MM-yy HH:mm").format(new Date().getTime());
+    Collection<BLRandomContract> selected;
+    DatabaseHelper db;
+    ArrayList<String> lablesSubVillages;
+    Collection<VillagesContract> SubVillagesList;
+    Map<String, String> SubVillagesMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_a);
+        bi.setCallback(this);
+        ValidatorClasss.setScrollViewFocus(bi.svseca);
+        db = new DatabaseHelper(this);
+        MainApp.members_f_m = new ArrayList<>();
+
+        MainApp.familyMembersList = new ArrayList<>();
+        MainApp.childUnder2 = new ArrayList<>();
+        MainApp.childUnder5 = new ArrayList<>();
+        MainApp.serial_no = 0;
+
+        lablesSubVillages = new ArrayList<>();
+        SubVillagesMap = new HashMap<>();
+        lablesSubVillages.add("Select Sub Village..");
+
+        SubVillagesList = db.getVillage(String.valueOf(MainApp.areaCode));
+
+        if (SubVillagesList.size() != 0) {
+            for (VillagesContract vil : SubVillagesList) {
+                lablesSubVillages.add(vil.getVillagename());
+                SubVillagesMap.put(vil.getVillagename(), vil.getVillagecode());
+            }
+        }
+
+        bi.spSubVillages.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, lablesSubVillages));
+
+        bi.spSubVillages.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (bi.spSubVillages.getSelectedItemPosition() != 0) {
+                    MainApp.cluster = SubVillagesMap.get(bi.spSubVillages.getSelectedItem().toString());
+
+                    bi.ta01.setText(MainApp.cluster);
+                    bi.ta06.setText(bi.spSubVillages.getSelectedItem().toString());
+                    bi.ta05h.setText(null);
+                } else {
+
+                    bi.ta01.setText(null);
+
+                    bi.ta06.setText("N/A");
+
+                    bi.fldGrpt03.setVisibility(View.GONE);
+
+                    bi.hhName.setText(null);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        bi.ta09.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                /*if (i == R.id.ta09a) {
+                    bi.btnContinue.setVisibility(View.VISIBLE);
+                    bi.fldGrpRespInfo.setVisibility(View.VISIBLE);
+                    bi.btnEnd.setVisibility(View.GONE);
+                } else {
+                    bi.btnContinue.setVisibility(View.GONE);
+                    bi.fldGrpRespInfo.setVisibility(View.GONE);
+                    bi.tc03.setText(null);
+                    bi.tc04.clearCheck();
+                    bi.tc05.setText(null);
+                    bi.btnEnd.setVisibility(View.VISIBLE);
+                }*/
+
+                if (i != bi.ta09a.getId())
+                    ClearClass.ClearAllFields(bi.fldGrpRespInfo, null);
+
+            }
+        });
+
+        bi.ta05h.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                bi.fldGrpt03.setVisibility(View.GONE);
+                bi.hhName.setText(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+//        Checkbox validate
+        bi.checkHHHeadpresent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    bi.fldGrpt03a.setVisibility(View.GONE);
+                    bi.newHHheadname.setText(null);
+                } else {
+                    bi.fldGrpt03a.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    public void onCheckHHClick() {
+
+        if (formValidation(false)) {
+            if (!db.checkFormAlreadyFilled(bi.ta01.getText().toString(), bi.ta05h.getText().toString().toUpperCase())) {
+
+                selected = db.getAllBLRandom(bi.ta01.getText().toString(), bi.ta05h.getText().toString().toUpperCase());
+
+                if (selected.size() != 0) {
+
+                    for (BLRandomContract rnd : selected) {
+                        MainApp.selectedHead = new BLRandomContract(rnd);
+                    }
+
+                    bi.hhName.setText(MainApp.selectedHead.getHhhead().toUpperCase());
+
+                    bi.fldGrpt03.setVisibility(View.VISIBLE);
+                } else {
+                    bi.hhName.setText(null);
+
+                    bi.fldGrpt03.setVisibility(View.GONE);
+
+                    Toast.makeText(this, "No Head found in this HH.", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                Toast.makeText(this, "This form is already filled!!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Not found.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void onBtnEndClick() {
+        if (formValidation(true)) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+
+                finish();
+                startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onBtnContinueClick() {
+        if (formValidation(true)) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+
+                finish();
+
+                startActivity(new Intent(this, SectionBActivity.class));
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void SaveDraft() throws JSONException {
+
+        SharedPreferences sharedPref = getSharedPreferences("tagName", MODE_PRIVATE);
+
+        MainApp.fc = new FormsContract();
+
+        MainApp.fc.setdevicetagID(sharedPref.getString("tagName", null));
+        MainApp.fc.setformDate(dtToday);
+        MainApp.fc.setuser(MainApp.userName);
+        MainApp.fc.setdeviceid(Settings.Secure.getString(getApplicationContext().getContentResolver(),
+                Settings.Secure.ANDROID_ID));
+        MainApp.fc.setappVer(MainApp.versionName + "." + MainApp.versionCode);
+        MainApp.fc.setcluster_no(bi.ta01.getText().toString());
+        MainApp.fc.sethhno(bi.ta05h.getText().toString());
+
+        JSONObject sa = new JSONObject();
+        MainApp.cluster = bi.ta01.getText().toString();
+        MainApp.hhno = bi.ta05h.getText().toString();
+
+        sa.put("sn", "monitor");
+        sa.put("rndid", MainApp.selectedHead.get_ID());
+        sa.put("luid", MainApp.selectedHead.getLUID());
+        sa.put("randDT", MainApp.selectedHead.getRandomDT());
+        sa.put("hh03", MainApp.selectedHead.getStructure());
+        sa.put("hh07", MainApp.selectedHead.getExtension());
+        sa.put("hhhead", MainApp.selectedHead.getHhhead());
+        sa.put("hhheadpresent", bi.checkHHHeadpresent.isChecked() ? "1" : "2");
+        sa.put("hhheadpresentnew", bi.newHHheadname.getText().toString());
+
+        sa.put("ta01", bi.ta01.getText().toString());
+        sa.put("ta02", bi.ta02a.isChecked() ? "1" : bi.ta02b.isChecked() ? "2" : bi.ta02c.isChecked() ? "3" : "0");
+        sa.put("ta03", MainApp.talukaCode);
+        sa.put("ta04", MainApp.ucCode);
+        sa.put("ta04A", MainApp.areaCode);
+        sa.put("ta05h", bi.ta05h.getText().toString());
+        sa.put("tc03", bi.tc03.getText().toString());
+        /*sa.put("tc04", bi.tc04a.isChecked() ? "1" : bi.tc04b.isChecked() ? "2" : "0");
+        sa.put("tc05", bi.tc05.getText().toString());*/
+        sa.put("ta06", bi.ta06.getText().toString());
+        /*sa.put("ta07", bi.ta07.getText().toString());
+        sa.put("ta08", bi.ta08.getText().toString());*/
+        sa.put("ta09", bi.ta09a.isChecked() ? "1" : bi.ta09b.isChecked() ? "2" : bi.ta09c.isChecked() ? "3" : "0");
+
+        MainApp.fc.setsA(String.valueOf(sa));
+        setGPS();
+
+    }
+
+    private boolean UpdateDB() {
+
+        Long updcount = db.addForm(MainApp.fc);
+        MainApp.fc.set_ID(String.valueOf(updcount));
+
+        if (updcount != 0) {
+
+            MainApp.fc.set_UID(
+                    (MainApp.fc.getdeviceid() + MainApp.fc.get_ID()));
+            db.updateFormID();
+
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public void setGPS() {
+        SharedPreferences GPSPref = getSharedPreferences("GPSCoordinates", Context.MODE_PRIVATE);
+
+        try {
+            String lat = GPSPref.getString("Latitude", "0");
+            String lang = GPSPref.getString("Longitude", "0");
+            String acc = GPSPref.getString("Accuracy", "0");
+            String dt = GPSPref.getString("Time", "0");
+
+            if (lat == "0" && lang == "0") {
+                Toast.makeText(this, "Could not obtained GPS points", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "GPS set", Toast.LENGTH_SHORT).show();
+            }
+
+            String date = DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(GPSPref.getString("Time", "0"))).toString();
+
+            MainApp.fc.setgpsLat(GPSPref.getString("Latitude", "0"));
+            MainApp.fc.setgpsLng(GPSPref.getString("Longitude", "0"));
+            MainApp.fc.setgpsAcc(GPSPref.getString("Accuracy", "0"));
+            MainApp.fc.setgpsDT(date); // Timestamp is converted to date above
+
+            Toast.makeText(this, "GPS set", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.e(TAG, "setGPS: " + e.getMessage());
+        }
+
+    }
+
+    public boolean formValidation(boolean flag) {
+
+        if (!ValidatorClasss.EmptyTextBox(this, bi.ta01, getString(R.string.ta01))) {
+            return false;
+        }
+        if (!ValidatorClasss.EmptyTextBox(this, bi.ta05h, getString(R.string.ta05h))) {
+            return false;
+        }
+        if (!ValidatorClasss.PatternTextBox(this, bi.ta05h, getString(R.string.ta05h), "[0-9]{4,4}-[^0-9]{1,1}", "XXXX-X")) {
+            return false;
+        }
+
+        if (flag) {
+            if (!bi.checkHHHeadpresent.isChecked()) {
+                if (!ValidatorClasss.EmptyTextBox(this, bi.newHHheadname, "New Head Name")) {
+                    return false;
+                }
+            }
+            if (!ValidatorClasss.EmptyRadioButton(this, bi.ta02, bi.ta02a, getString(R.string.ta02))) {
+                return false;
+            }
+            if (!ValidatorClasss.EmptyTextBox(this, bi.ta06, getString(R.string.ta06))) {
+                return false;
+            }
+            /*if (!ValidatorClasss.EmptyTextBox(this, bi.ta07, getString(R.string.ta07))) {
+                return false;
+            }
+            if (!ValidatorClasss.EmptyTextBox(this, bi.ta08, getString(R.string.ta08))) {
+                return false;
+            }*/
+            if (!ValidatorClasss.EmptyRadioButton(this, bi.ta09, bi.ta09a, getString(R.string.ta09))) {
+                return false;
+            }
+            if (bi.ta09a.isChecked()) {
+                return ValidatorClasss.EmptyTextBox(this, bi.tc03, getString(R.string.tc03));
+                /*if (!ValidatorClasss.EmptyRadioButton(this, bi.tc04, bi.tc04a, getString(R.string.tc04))) {
+                    return false;
+                }
+                if (!ValidatorClasss.EmptyTextBox(this, bi.tc05, getString(R.string.tc05))) {
+                    return false;
+                }
+                if (!ValidatorClasss.RangeTextBox(this, bi.tc05, 14, 99, getString(R.string.tc05), " years")) {
+                    return false;
+                }
+                int ageofResp = Integer.parseInt(bi.tc05.getText().toString());
+                if (ageofResp < 14) {
+                    Toast.makeText(this, "Respondant age must be geater then 14 years", Toast.LENGTH_LONG);
+                    bi.tc05.setError("Respondant age must be geater then 14 years");
+                    return false;
+                } else {
+                    bi.tc05.setError(null);
+                }*/
+
+            }
+
+        }
+
+        return true;
+    }
+}
+
---
+++ app/src/main/java/edu/aku/ramshasaeed/tmk_midline_monitor19/contracts/FamilyMembersContract.java
@@ -0,0 +1,350 @@
+package edu.aku.ramshasaeed.tmk_midline_monitor19.contracts;
+
+import android.database.Cursor;
+import android.provider.BaseColumns;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FamilyMembersContract {
+
+    private final String projectName = "UEN-TMK-MIDLINE";
+    private String _ID = "";
+    private String _UID = "";
+    private String _UUID = "";
+    private String formDate = "";
+    private String deviceId = "";
+    private String user = "";
+    private String devicetagID = "";
+    private String name = "";
+    private String dob = "";
+    private String age = "";
+    private String sB = "";
+    private String synced = "";
+    private String syncedDate = "";
+    private String istatus = "";
+    private String serialNo = "";
+    private String motherId = "";
+    private String type = "";
+    private String app_ver = "";
+    private String clusterNo = "";
+    private String hhNo = "";
+
+    private double ageDouble;
+
+
+
+    public FamilyMembersContract() {
+    }
+
+    public FamilyMembersContract(String name, String age, String serialNo, String dob) {
+        this.name = name;
+
+        this.age = age;
+//        this.ageLess2 = ageLess2;
+        this.serialNo = serialNo;
+        this.dob = dob;
+    }
+
+    public FamilyMembersContract(FamilyMembersContract fm) {
+        this.name = fm.name;
+        this.age = fm.age;
+        this.serialNo = fm.serialNo;
+        this.dob = fm.dob;
+    }
+
+    public double getAgeDouble() {
+        return ageDouble;
+    }
+
+    public void setAgeDouble(double ageDouble) {
+        this.ageDouble = ageDouble;
+    }
+
+    public String getprojectName() {
+        return projectName;
+    }
+
+    public String get_ID() {
+        return _ID;
+    }
+
+    public void set_ID(String _ID) {
+        this._ID = _ID;
+    }
+
+    public String get_UID() {
+        return _UID;
+    }
+
+    public void set_UID(String _UID) {
+        this._UID = _UID;
+    }
+
+    public String get_UUID() {
+        return _UUID;
+    }
+
+    public void set_UUID(String _UUID) {
+        this._UUID = _UUID;
+    }
+
+    public String getformDate() {
+        return formDate;
+    }
+
+    public void setformDate(String formDate) {
+        this.formDate = formDate;
+    }
+
+    public String getdeviceId() {
+        return deviceId;
+    }
+
+    public void setdeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getuser() {
+        return user;
+    }
+
+    public void setuser(String user) {
+        this.user = user;
+    }
+
+    public String getdevicetagID() {
+        return devicetagID;
+    }
+
+    public void setdevicetagID(String devicetagID) {
+        this.devicetagID = devicetagID;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getdob() {
+        return dob;
+    }
+
+    public void setdob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getage() {
+        return age;
+    }
+
+    public void setage(String age) {
+        this.age = age;
+    }
+
+    public String getsB() {
+        return sB;
+    }
+
+    public void setsB(String sB) {
+        this.sB = sB;
+    }
+
+    public String getsynced() {
+        return synced;
+    }
+
+    public void setsynced(String synced) {
+        this.synced = synced;
+    }
+
+    public String getsyncedDate() {
+        return syncedDate;
+    }
+
+    public void setsyncedDate(String syncedDate) {
+        this.syncedDate = syncedDate;
+    }
+
+    public String getistatus() {
+        return istatus;
+    }
+
+    public void setistatus(String istatus) {
+        this.istatus = istatus;
+    }
+
+    public String getserialNo() {
+        return serialNo;
+    }
+
+    public void setserialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getmotherId() {
+        return motherId;
+    }
+
+    public void setmotherId(String motherId) {
+        this.motherId = motherId;
+    }
+
+    public String gettype() {
+        return type;
+    }
+
+    public void settype(String type) {
+        this.type = type;
+    }
+
+    public String getapp_ver() {
+        return app_ver;
+    }
+
+    public void setapp_ver(String app_ver) {
+        this.app_ver = app_ver;
+    }
+
+    public String getclusterNo() {
+        return clusterNo;
+    }
+
+    public void setclusterNo(String clusterNo) {
+        this.clusterNo = clusterNo;
+    }
+
+    public String gethhNo() {
+        return hhNo;
+    }
+
+    public void sethhNo(String hhNo) {
+        this.hhNo = hhNo;
+    }
+
+    public FamilyMembersContract Sync(JSONObject jsonObject) throws JSONException {
+
+        this._ID= jsonObject.getString(familyMembers.COLUMN_ID);
+        this._UID= jsonObject.getString(familyMembers.COLUMN_UID);
+        this._UUID= jsonObject.getString(familyMembers.COLUMN_UUID);
+        this.formDate= jsonObject.getString(familyMembers.COLUMN_FORMDATE);
+        this.deviceId= jsonObject.getString(familyMembers.COLUMN_DEVICEID);
+        this.user= jsonObject.getString(familyMembers.COLUMN_USER);
+        this.devicetagID= jsonObject.getString(familyMembers.COLUMN_DEVICETAGID);
+        this.name= jsonObject.getString(familyMembers.COLUMN_NAME);
+        this.dob= jsonObject.getString(familyMembers.COLUMN_DOB);
+        this.age= jsonObject.getString(familyMembers.COLUMN_AGE);
+        this.sB= jsonObject.getString(familyMembers.COLUMN_SB);
+        this.synced= jsonObject.getString(familyMembers.COLUMN_SYNCED);
+        this.syncedDate= jsonObject.getString(familyMembers.COLUMN_SYNCED_DATE);
+        this.istatus= jsonObject.getString(familyMembers.COLUMN_ISTATUS);
+        this.serialNo= jsonObject.getString(familyMembers.COLUMN_SERIALNO);
+        this.motherId= jsonObject.getString(familyMembers.COLUMN_MOTHERID);
+        this.type= jsonObject.getString(familyMembers.COLUMN_TYPE);
+        this.app_ver= jsonObject.getString(familyMembers.COLUMN_APP_VER);
+        this.clusterNo= jsonObject.getString(familyMembers.COLUMN_CLUSTERNO);
+        this.hhNo= jsonObject.getString(familyMembers.COLUMN_HHNO);
+
+
+        return this;
+
+    }
+
+    public FamilyMembersContract Hydrate(Cursor cursor) {
+        this._ID = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_ID));
+        this._UID = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_UID));
+        this._UUID = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_UUID));
+        this.formDate = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_FORMDATE));
+        this.deviceId = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_DEVICEID));
+        this.user = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_USER));
+        this.devicetagID = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_DEVICETAGID));
+        this.name = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_NAME));
+        this.dob = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_DOB));
+        this.age = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_AGE));
+        this.sB = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_SB));
+        this.synced = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_SYNCED));
+        this.syncedDate = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_SYNCED_DATE));
+        this.istatus = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_ISTATUS));
+        this.serialNo = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_SERIALNO));
+        this.motherId = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_MOTHERID));
+        this.type = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_TYPE));
+        this.app_ver = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_APP_VER));
+        this.clusterNo = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_CLUSTERNO));
+        this.hhNo = cursor.getString(cursor.getColumnIndex(familyMembers.COLUMN_HHNO));
+
+        return this;
+
+    }
+
+
+    public JSONObject toJSONObject() throws JSONException {
+
+        JSONObject json = new JSONObject();
+        json.put(familyMembers.COLUMN_PROJECT_NAME, this.projectName == null ? JSONObject.NULL : this.projectName);
+        json.put(familyMembers.COLUMN_ID, this._ID == null ? JSONObject.NULL : this._ID);
+        json.put(familyMembers.COLUMN_UID, this._UID == null ? JSONObject.NULL : this._UID);
+        json.put(familyMembers.COLUMN_UUID, this._UUID == null ? JSONObject.NULL : this._UUID);
+        json.put(familyMembers.COLUMN_FORMDATE, this.formDate == null ? JSONObject.NULL : this.formDate);
+        json.put(familyMembers.COLUMN_DEVICEID, this.deviceId == null ? JSONObject.NULL : this.deviceId);
+        json.put(familyMembers.COLUMN_USER, this.user == null ? JSONObject.NULL : this.user);
+        json.put(familyMembers.COLUMN_DEVICETAGID, this.devicetagID == null ? JSONObject.NULL : this.devicetagID);
+        json.put(familyMembers.COLUMN_NAME, this.name == null ? JSONObject.NULL : this.name);
+        json.put(familyMembers.COLUMN_DOB, this.dob == null ? JSONObject.NULL : this.dob);
+        json.put(familyMembers.COLUMN_AGE, this.age == null ? JSONObject.NULL : this.age);
+        if (!this.sB.equals("")) {
+
+            json.put(familyMembers.COLUMN_SB, this.sB.equals("") ? JSONObject.NULL : new JSONObject(this.sB));
+        }
+        json.put(familyMembers.COLUMN_ISTATUS, this.istatus == null ? JSONObject.NULL : this.istatus);
+        json.put(familyMembers.COLUMN_SERIALNO, this.serialNo == null ? JSONObject.NULL : this.serialNo);
+        json.put(familyMembers.COLUMN_MOTHERID, this.motherId == null ? JSONObject.NULL : this.motherId);
+        json.put(familyMembers.COLUMN_TYPE, this.type == null ? JSONObject.NULL : this.type);
+        json.put(familyMembers.COLUMN_APP_VER, this.app_ver == null ? JSONObject.NULL : this.app_ver);
+        json.put(familyMembers.COLUMN_CLUSTERNO, this.clusterNo == null ? JSONObject.NULL : this.clusterNo);
+        json.put(familyMembers.COLUMN_HHNO, this.hhNo == null ? JSONObject.NULL : this.hhNo);
+
+
+
+        return json;
+    }
+
+    public static abstract class familyMembers implements BaseColumns {
+
+        public static final String TABLE_NAME = "familymembers";
+        public static final String COLUMN_NAME_NULLABLE = "NULLHACK";
+
+        public static final String COLUMN_PROJECT_NAME = "project_name";
+
+
+        public static final String COLUMN_ID = "_id";
+        public static final String COLUMN_UID = "uid";
+        public static final String COLUMN_UUID = "uuid";
+        public static final String COLUMN_FORMDATE = "formdate";
+        public static final String COLUMN_DEVICEID = "deviceid";
+        public static final String COLUMN_USER = "user";
+        public static final String COLUMN_DEVICETAGID = "tagid";
+
+        //Adjust this
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_DOB = "dob";
+        public static final String COLUMN_AGE = "age";
+
+        public static final String COLUMN_SB = "sB";
+        public static final String COLUMN_SYNCED = "synced";
+        public static final String COLUMN_SYNCED_DATE = "sync_date";
+        public static final String COLUMN_ISTATUS = "istatus";
+
+        //Adjust this
+        public static final String COLUMN_SERIALNO = "serialno";
+        public static final String COLUMN_MOTHERID = "motherid";
+        public static final String COLUMN_TYPE = "type";
+        public static final String COLUMN_APP_VER = "app_ver";
+        public static final String COLUMN_CLUSTERNO = "clusterno";
+        public static final String COLUMN_HHNO = "hhno";
+
+        public static String _URL = "familymembersmonitor.php";
+    }
+}
